2353b436d105bb1580214a5732a6153a3df6db68
ZA <atl@elx3labs.com>
2021-03-16 17:30:42 +1100

    Updated

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,82 +1,39 @@
 
-// $(document).ready(function(){
-//     body.addEventListener("load", init(), false);
-//     $('.ui.dropdown').dropdown();  
-//     document.getElementsByClassName("ui red button")[0].addEventListener("click", recordVote);
-//   });
-
-window.addEventListener('load', (event) => {
-    console.log('The page has fully loaded');
-    refreshDashboard();
+$(document).ready(function(){
+  refreshStatCounts();
+  // document.getElementById("refreshstats").addEventListener("click", test);
 });
 
-//   var vote_endpoint = "https://EXAMPLE_REPLACE_ME.execute-api.us-east-1.amazonaws.com/dev/song/vote"
-//   var get_votes_endpoint = "https://EXAMPLE_REPLACE_ME.execute-api.us-east-1.amazonaws.com/dev/votes"
-  
-var dashboard_endpoint = "https://4qe6u3vj45.execute-api.ap-southeast-2.amazonaws.com/dev/stats/injuries"
+//   setInterval(refreshStatCounts, 500, )
 
-//   function setVotes(songName, voteCount) {
-//     // Get div containing vote count and set the new voteCount
-//     document.getElementsByName(songName)[0].innerHTML = voteCount;
-//   }
+var get_stats_endpoint = "https://2kgkqn4ohc.execute-api.ap-southeast-2.amazonaws.com/dev/stats"
 
-async function refreshDashboard() {
-    // Get the dashboard statistics
-    const response = await fetch(dashboard_endpoint, {
-        method: "GET",
-        mode: 'cors',
-        headers: {'Content-Type': 'application/json'},
-        // body: JSON.stringify({"songName": songName})
-    });
-        
-    const stats = await response.json();
+// function refreshstats() {
+//   console.log("Refresh Stats...")
+// }
 
-    // Iterate over all three statistics and update the divs
-    var i;
-    for (i = 0; i < stats.length; i++) {
-    //  var featured_songs = ["coderitis", "stateless", "dynamo"];
-        var stat = stats[i];
-        console.log(stat);
-    //   if (featured_songs.includes(song["songName"])){
-    //     console.log(song)
-    //     setVotes(song["songName"], song["votes"])
-    //   }
-    }
+function setStats(statName, statValue) {
+  // Get div containing stat count and set the new statValue
+  document.getElementsByClassName(statName)[0].innerHTML = statValue;
 }
 
-//   async function refreshVoteCounts() {
-//     // Get the vote counts
-//     const response = await fetch(get_votes_endpoint);
-//     const songs = await response.json();
-//     // Iterate over all three songs and update the divs
-//     var i;
-//     for (i = 0; i < songs.length; i++){
-//       var featured_songs = ["coderitis", "stateless", "dynamo"];
-//       var song = songs[i]
-//       if (featured_songs.includes(song["songName"])){
-//         console.log(song)
-//         setVotes(song["songName"], song["votes"])
-//       }
-//     }
-//   }
-  
-//   async function voteForSong(songName) {
-//     const response = await fetch(vote_endpoint, {
-//       method: "POST",
-//       mode: 'cors',
-//       headers: {'Content-Type': 'application/json'},
-//       body: JSON.stringify({"songName": songName})
-//     })
-//     const result_json = await response.json()
-//     setVotes(songName, result_json["votes"])
-//   }
-  
-//   function recordVote() {
-//     if (document.getElementsByClassName("item active selected")[0]) {
-//       var selectedSong = document.getElementsByClassName("item active selected")[0].getAttribute('data-value')
-//       if (selectedSong) {
-//         voteForSong(selectedSong)
-//         console.log("voted for " + selectedSong)
-//       }
-//     }
-//   }
+async function refreshStatCounts() {
+  // Get the stat counts
+  const response = await fetch(get_stats_endpoint);
+  const stats = await response.json();
+  // Iterate over all three featured stats and update the values
+  var i;
+  for (i = 0; i < stats.length; i++){
+    // console.log(stats[i])
+    var tracked_stats = ["deaths", "accidents", "injuries"];
+    var stat = stats[i]
+    if (tracked_stats.includes(stat["statName"])){
+      console.log(stat)
+      setStats(stat["statName"], stat["statValue"])
+    }
+  }
+}
+
+function recordStat() {
+  console.log("Refreshed...")
+}
